04df3d4cdcff9fe77293bbb4f1b787706ca36f6f
Yeabsira Mekuria <yabumek46@gmail.com>
2023-02-24 13:44:21 +0300

    clone basic features from blogester

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,26 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
-import tailwind from "@astrojs/tailwind";
+import tailwind from "@astrojs/tailwind"
+
+
+const SERVER_PORT = 3000
+// the url to access your blog during local development
+const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`
+// the url to access your blog after deploying it somewhere (Eg. Netlify)
+const LIVE_URL = "https://yourwebsiteurl.com"
+// this is the astro command your npm script runs
+const SCRIPT = process.env.npm_lifecycle_script || ""
+const isBuild = SCRIPT.includes("astro build")
+let BASE_URL = LOCALHOST_URL
+// When you're building your site in local or in CI, you could just set your URL manually
+if (isBuild) {
+  BASE_URL = LIVE_URL
+}
 
 // https://astro.build/config
 export default defineConfig({
+  server: { port: SERVER_PORT },
+  site: BASE_URL,
   integrations: [tailwind()]
-});
+})
